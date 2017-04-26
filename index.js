2f7d311fc8ddb912efe11ffc882f60397af3a7a1
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-26 19:48:10 +0700

    start

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('SERVER START'));
 
-app.get('/', (req, res) => res.send('HELLO'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => res.render('home'));
