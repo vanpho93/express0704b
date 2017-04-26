a551b108051056aad7337e7421c16d3541b8dea3
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-26 20:35:16 +0700

    pt

--- index.js
+++ index.js
@@ -22,4 +22,37 @@ app.get('/tinh', (req, res) => {
 
 app.post('/xuly', parser, (req, res) => {
     //TODO HERE
+    const { soa, sob, pt } = req.body;
+    const phepTinh = new PhepTinh(soa, soa, pt);
+    res.send(phepTinh.show());
 });
+
+class PhepTinh {
+    constructor(soA, soB, tenPhepTinh) {
+        this.soA = soA;
+        this.soB = soB;
+        this.tenPhepTinh = tenPhepTinh;
+    }
+
+    show() {
+        let pt;
+        switch (this.tenPhepTinh) {
+            case 'cong': { 
+                pt = '+';
+                break;
+            }
+            case 'tru': { 
+                pt = '-';
+                break;
+            }case 'nhan': { 
+                pt = '*';
+                break;
+            }default: { 
+                pt = '/';
+                break;
+            }
+        }
+        const veTrai = `${this.soA} ${pt} ${this.soB}`;
+        return `${veTrai} = ${ eval(veTrai) }`;
+    }
+}
